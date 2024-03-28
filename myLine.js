a0b0df00ca73080743af7b7964e029c9d3fb7278
fawkesa <159979653+fawkesa@users.noreply.github.com>
2024-03-27 19:42:11 -0700

    Add files via upload

--- myLine.js
+++ myLine.js
@@ -1,12 +1,19 @@
+/* Created a line object for every line that is drawn. 
+The line object needs 4 values: mouse's previous x & y & current x & y.
+Line color is a property to the line object
+Created a method that shows the line.
+*/
 class myLine {
-    constructor() {
+    constructor(lineColor) {
         this.px = pwinMouseX
         this.py = pwinMouseY
         this.x = winMouseX
         this.y = winMouseY
+
+        this.lineColor = lineColor
     }
     show() {
-       stroke('orange')
+       stroke(this.lineColor)
        line(this.px, this.py, this.x, this.y)
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,30 +1,72 @@
+//Created empty array to keep track of lines
 var lines = []
-var lineColor
+var lineColor = 'green'
 var clearBut
+var previousDistance = 1000
+previousColor = 'green'
 
 function setup() {
   createCanvas(400, 400)
-  
+  frameRate(3)
+  //Create button to clear canvas
   var options = createDiv().style('display: flex')
   clearBut = createButton('Clear').parent(options).style('width: 100px')
 
 }
 
   function draw() { 
-    background('green')
+    background('gray')
+    createStar()
 
-    //clears line
+         //color change testing :D 
+         x = 200
+         y = 100
+         currentDistance = Math.sqrt((Math.pow(winMouseX - x, 2) + Math.pow(winMouseY - y, 2)), 2)
+         if (currentDistance < previousDistance) {
+           lineColor = 'green'
+         } else {
+            if (previousColor == 'green') {
+             lineColor = 'yellow'
+            }
+            if (previousColor == 'yellow'){
+             lineColor = 'red'
+            }
+           }
+   
+
+    //Clears line from canvas
     clearBut.mousePressed(function() {
       lines = []
     })
 
-    //draws line is mouse is pressed
+    //Draws line (adds line object to the array) if mouse is pressed
     if (mouseIsPressed) {
-      var line = new myLine()
+      var line = new myLine(lineColor.valueOf())
       lines.push(line)
     }
 
     for(var line of lines) {
       line.show()
     } 
+
+    previousDistance = currentDistance
+    previousColor = lineColor
 } 
+
+
+function createStar() {
+  stroke('black')
+  fill('black')
+  circle(200, 100, 10)
+  circle(325, 200, 10)
+  circle(275, 350, 10)
+  circle(125, 350, 10)
+  circle(75, 200, 10)
+}
+
+/* Old Code
+function mouseToGoalDot(int x, int y) {
+  distance = Math.sqrt((Math.pow(winMouseX - x, 2) + Math.pow(winMouseY - y, 2)), 2)
+  return distance
+} 
+*/
